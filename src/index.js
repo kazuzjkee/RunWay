1bea94861ab2163dbc30d2bbc46fa2dedfe18ca7
kazuzjkee <egorlitwinoff@gmail.com>
2025-06-17 17:19:11 +0300

    mini fix

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <SpeedInsights />
     <App />
   </React.StrictMode>
 );
