5aa4d1b5fd6f84195481b6471c44eabe1436a35f
kazuzjkee <egorlitwinoff@gmail.com>
2025-06-18 04:27:40 +0300

    add favicon

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import "./styles/index.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
+  <HashRouter>
     <App />
-  </Router>
+  </HashRouter>
 );
